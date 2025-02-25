7ee1b2e577b15b0629f67549808bdb55cfd8a738
juancrfig <juancrfig@gmail.com>
2025-02-25 10:22:31 -0500

    Finish project

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
+import { generateDayInformation } from "./modules/dayInformation.js";
+
+const mainElement = document.querySelector('#mainEl');
+
 const API_KEY = 'XVBJRDCXSCJ5Y4QCW65MXQ9MF';
-// const URL_FORMAT = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
-const urlLondon = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London, UK?key=${API_KEY}`;
 const searchElement = document.querySelector('#searchInput');
 
 
-searchElement.addEventListener('keydown', (event) => {
+searchElement.addEventListener('keydown', async (event) => {
     if (event.key === 'Enter') {
         const userResponse = searchElement.value;
-        console.log(userResponse);
+        const response = await getWeatherData(userResponse);
+        const data = await processData(response);
+        displayInformation(data);
+        
     }
 } )
 
-async function getWeatherData() {
+function displayInformation(data) {
+    mainElement.innerHTML = `<h1 class="text-white text-3xl font-bold">Weather in ${data[0]}</h1>
+    <p class="pt-5 pb-10 text-2xl">${data[1]}</p>`;
+
+    const flexContainer = document.createElement('div');
+    flexContainer.setAttribute('class', 'flex flex-wrap justify-center');
+    mainElement.appendChild(flexContainer);
+
+    data[2].forEach( day => {
+        flexContainer.innerHTML += generateDayInformation(day.date, day.temp, day.conditions, day.tempmax, day.tempmin, day.uvindex);
+    });
+}
+
+
+async function getWeatherData(userResponse) {
+    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userResponse}?key=${API_KEY}`;
     try {
-        const response = await fetch(urlLondon);
+        const response = await fetch(URL);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -22,19 +42,16 @@ async function getWeatherData() {
 
     } catch (error){
         console.error('Error fetching weather data:', error);
+        alert('No data about the city you typed :(')
         return null;
     }
 }
 
-async function consoleData() {
-    const data = await getWeatherData();
-    processData(data)
-}
-
 async function processData(data) {
 
-    const address = data.address;
     const description = data.description;
+    const resolvedAddress = data.resolvedAddress;
+
     const days = [];
     data.days.forEach(day => {
         if (days.length > 6) {
@@ -51,19 +68,5 @@ async function processData(data) {
             }
         );
     });
-    console.log(address)
-    console.log(description)
-    console.log(days)
+    return [resolvedAddress, description, days];
 }
-
-consoleData();
-
-
-
-
-async function main(){
-
-
-} 
-
-main();
